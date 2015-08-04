414bcacdc3c3b119a8f75da9cbefcf4a1f9045e8
Chetan Kantharia <chetan.kantharia@gmail.com>
2015-08-04 18:51:13 +0530

    Restructured

--- app.js
+++ app.js
@@ -5,6 +5,94 @@ var cheerio    = require('cheerio');
 var cors       = require('cors');
 var bodyParser = require('body-parser');
 
+/*
+	Helper functions
+*/
+var getMeta = {
+	getResponseHeader : function(response, key){
+		return response.headers && response.headers[key];
+	},
+
+	getContentType : function(response){
+		return this.getResponseHeader(response, 'content-type')
+	},
+
+	isContentTypeValid : function(response, type){
+		/* Hash Map of all the valid patterns */
+		var patterns = {
+			'html' : 'text\/html'
+		}
+
+		var checkContentType = new RegExp(patterns[type]);
+		return checkContentType.test(this.getContentType(response))
+	},
+
+	getMetaTag : function(body, URL){
+		var $ = cheerio.load(body);
+
+		var title = $('title');
+		var meta  = $('meta');
+		var keys  = Object.keys(meta);
+
+		//Init default title and description
+		var defaultTitle = title[0] && title[0].children[0] && title[0].children[0].data;
+		var defaultDescription;
+		var defaultUrl = defaultTitle && URL;
+
+		var metaTag = {};
+
+		keys.forEach(function(key){
+
+			//extract description
+			if(meta[key].attribs && meta[key].attribs.name === 'description'){
+				defaultDescription = meta[key].attribs.content;
+			}
+
+		  if (meta[key].attribs && meta[key].attribs.property) {
+
+		  	if(meta[key].attribs.property === 'og:title'){
+		  		metaTag.title = meta[key].attribs.content;
+		  	}
+
+		  	if(meta[key].attribs.property === 'og:url'){
+		  			metaTag.url = meta[key].attribs.content;
+		  	}
+
+		  	if(meta[key].attribs.property === 'og:description'){
+		  		metaTag.description = meta[key].attribs.content;
+		  	}
+
+		  	if(meta[key].attribs.property === 'og:image'){
+		  		//check http or https prefix for image url
+		  		var checkPrefix = new RegExp('http|https');
+		  		if(checkPrefix.test(meta[key].attribs.content)){
+		  			metaTag.image = meta[key].attribs.content;
+		  			console.log('Image', metaTag.image);
+		  		} else {
+		  			metaTag.image = DOMAIN + meta[key].attribs.content;
+		  			console.log('Image', metaTag.image);
+		  		}
+		  	}
+		  }
+		});
+
+		//Set default title and desc
+		if(!metaTag.title && defaultTitle){
+			metaTag.title = defaultTitle;
+		}
+		if(!metaTag.description && defaultDescription){
+			metaTag.description = defaultDescription;
+		}
+		if(!metaTag.url && defaultUrl){
+			metaTag.url = defaultUrl;
+		}
+		
+		return metaTag;
+	}
+
+};
+
+
 app.use(express.static(__dirname + "/public"));
 
 app.options(cors());
@@ -37,94 +125,32 @@ app.post('/v1', function(req, res){
 		//Send a HTTP Request to url to fetch html page
 		request(URL, function(error, response, body){
 
-			console.log('fetching : ', URL);
-			console.log('response : ', response.headers['content-type']);
-
-			//regex to check content-type = text/html
-			var checkContentType = new RegExp('text\/html');
-			if(checkContentType.test(response.headers['content-type'])){
-				contentIsHTML = true;
-			} else {
-				contentIsHTML = false;
-				res.status(500).json({
-					status : 500,
-					error : "Error occured while fetching url, content-type should be text/html",
-					url : URL
-				});
-			}
-
-			//Error on fetching HTML page 
+			/* Check for error */
 			if(error){
 				res.status(500).json({
 					status : 500,
 					error : "Error occured while fetching url",
 					url : URL
 				});
-			}
+			} else {
 
-			// On Response Body
-			if(body && contentIsHTML){
-				var $ = cheerio.load(body);
+				var contentIsHTML = getMeta.isContentTypeValid(response, 'html');
 
-				var title = $('title');
-				var meta  = $('meta');
-				var keys  = Object.keys(meta);
+				if(contentIsHTML && body){
+					
+					/* Cheerio Starts Here */
+					var metaTag = getMeta.getMetaTag(body, URL);
 
-				//Init default title and description
-				var defaultTitle = title[0] && title[0].children[0].data;
-				var defaultDescription;
-				var defaultUrl = defaultTitle && URL;
-
-				var metaTag = {};
-
-				keys.forEach(function(key){
-
-					//extract description
-					if(meta[key].attribs && meta[key].attribs.name === 'description'){
-						defaultDescription = meta[key].attribs.content;
-					}
-
-				  if (meta[key].attribs && meta[key].attribs.property) {
-
-				  	if(meta[key].attribs.property === 'og:title'){
-				  		metaTag.title = meta[key].attribs.content;
-				  	}
-
-				  	if(meta[key].attribs.property === 'og:url'){
-				  			metaTag.url = meta[key].attribs.content;
-				  	}
-
-				  	if(meta[key].attribs.property === 'og:description'){
-				  		metaTag.description = meta[key].attribs.content;
-				  	}
-
-				  	if(meta[key].attribs.property === 'og:image'){
-				  		//check http or https prefix for image url
-				  		var checkPrefix = new RegExp('http|https');
-				  		if(checkPrefix.test(meta[key].attribs.content)){
-				  			metaTag.image = meta[key].attribs.content;
-				  			console.log('Image', metaTag.image);
-				  		} else {
-				  			metaTag.image = DOMAIN + meta[key].attribs.content;
-				  			console.log('Image', metaTag.image);
-				  		}
-				  	}
-				  }
-				});
-
-					//Set default title and desc
-					if(!metaTag.title && defaultTitle){
-						metaTag.title = defaultTitle;
-					}
-					if(!metaTag.description && defaultDescription){
-						metaTag.description = defaultDescription;
-					}
-					if(!metaTag.url && defaultUrl){
-						metaTag.url = defaultUrl;
-					}
-
-				res.status(200)
-					 .json(metaTag);
+					res.status(200)
+						.json(metaTag);
+				} else {
+					console.log('Content is not HTML');
+					res.status(500).json({
+						status : 500,
+						error : "Error content is not html",
+						url : URL
+					});
+				}
 			}
 		})
 });
